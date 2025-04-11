b480c21ba9ea8e3428c0726eb07a709578fb110d
kcchen107 <c.karen1@wustl.edu>
2025-04-10 21:05:46 -0500

    added detail to button description

--- src/App.jsx
+++ src/App.jsx
@@ -202,7 +202,7 @@ const App = () => {
 
         <div id="coolQuote">
           <button id="clickInspo" onClick={newQuote}>
-            <h3>Click for inspiration</h3>
+            <h3>We offer our services around the world. Click for an example location.</h3>
           </button>
 
           <div className="card" id="inspireQuote">
